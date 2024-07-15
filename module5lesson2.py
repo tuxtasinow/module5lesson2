dc39eecd9ef88e5510ec7167f3f0385ab0917053
Bakhtiyar Tukhtasinov <144336544+tuxtasinow@users.noreply.github.com>
2024-07-15 18:01:39 +0500

    Add files via upload

---
+++ module5lesson2.py
@@ -0,0 +1,35 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self,new_floor: int):
+        if new_floor > self.number_of_floors or new_floor < 1:
+                print('Такого этажа не существует')
+        else:
+            for i in range(1, new_floor + 1):
+                print(i)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, количество этажей: {self.number_of_floors}'
+
+
+#h1 = House('ЖК Горский', 18)
+#h2 = House('Домик в деревне', 2)
+#h1.go_to(5)
+#h2.go_to(10)
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
